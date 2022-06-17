f1b31e1f0c8dbe370539f4e36412400ec7d8c9b5
Arthur Santiago <arthursantaguiar@gmail.com>
2022-06-17 16:55:19 -0300

    Developed project entities

---
+++ src/main/java/br/com/springboot/bookstoremanager/entities/Book.java
@@ -0,0 +1,51 @@
+package br.com.springboot.bookstoremanager.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// fornece os getters e setters
+@Data
+
+// Fornece uma classe no padrao de projetos builde, na crição de projetos seguros.
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String name;
+
+  @Column(nullable = false)
+  private Integer page;
+
+  @Column(nullable = false)
+  private Integer chapter;
+
+  @Column(nullable = false)
+  private String isbn;
+
+  @Column(nullable = false, name = "publisher_name", unique = true)
+  private String publisherName;
+
+  @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+  @JoinColumn(name = "author_id")
+  private Author author;
+
+}
